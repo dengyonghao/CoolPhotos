9bb2d97f8410963b39cecb7bf35c617cbafe7dcf
dengyonghao <dengyh@jpush.cn>
2016-07-29 14:56:24 +0800

    Initial Commit

---
+++ CoolPhotos/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  CoolPhotos
+//
+//  Created by 邓永豪 on 16/7/29.
+//  Copyright © 2016年 DYH. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
